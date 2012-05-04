557cc16d37d72a4707dd7870395b8b8b831ca71f
Ian Schneider <planablediglet@gmail.com>
2012-05-04 15:16:05 -0600

    add noscript and older IE versions warning

--- context_processors.py
+++ context_processors.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from mapstory.models import *
 from mapstory.util import lazy_context
 
+import re
+
 @lazy_context
 def _sections():
     return list(Section.objects.order_by('order'))
@@ -11,8 +13,28 @@ def _sections():
 def _resources():
     return list(Resource.objects.order_by('order'))
 
+_ua = re.compile('MSIE (\d+)\.')
+
 def page(req):
     '''provide base template context'''
+    # detect old browsers - probably should fix to use more robust framework
+    # for now, IE7 causes lots of trouble
+    old_browser = False
+    agent = req.META.get('HTTP_USER_AGENT','')
+    if agent:
+        match = _ua.search(agent)
+        if match:
+            try:
+                version = int(match.group(1))
+                old_browser = version < 8
+            except:
+                pass
+        if old_browser:
+            print req.COOKIES
+            # the user has understood
+            if 'iunderstand' in req.COOKIES:
+                old_browser = False
+    
     #@todo better of using a template_tag
     page = {
         'sections' : _sections(),
@@ -20,6 +42,7 @@ def page(req):
         #@todo temp for design work
         'design_mode' : settings.DESIGN_MODE,
         # we could do this through debug but it's nicer to have finer grained control
-        'enable_analytics' : settings.ENABLE_ANALYTICS
+        'enable_analytics' : settings.ENABLE_ANALYTICS,
+        'old_browser' : old_browser
     }
     return {'page':page,'cache_time':60}
