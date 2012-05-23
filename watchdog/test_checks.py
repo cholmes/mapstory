33aab505a5885d47889cd473568ee1e08fc52109
Ian Schneider <planablediglet@gmail.com>
2012-05-23 14:01:42 -0600

    add test

---
+++ watchdog/test_checks.py
@@ -0,0 +1,32 @@
+from watchdog.core import *
+
+def suite():
+    return (
+        test_single,
+        test_single_fail,
+        test_many,
+        test_restart
+    )
+    
+@check
+def test_single():
+    print 'test_single'
+
+@check
+def test_single_fail():
+    print 'test_single_fail'
+    raise Exception('dang')
+
+@check
+def test_many():
+    print 'test_many'
+    def test(i):
+        print 'test_many %s' % i
+        pass
+    for i in range(5):
+        yield subcheck(test,'subtest',i)
+
+@check(restart_on_error=True)
+def test_restart():
+    print 'test_restart'
+    raise Exception('failure')
