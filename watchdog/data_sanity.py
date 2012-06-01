801dae14a55f9ac0eadac25b2adb4429e1d3ae3b
Robert Marianski <rob@marianski.com>
2012-06-01 16:52:04 -0400

    add check for latLonBoundingBox in featuretype.xml

--- watchdog/data_sanity.py
+++ watchdog/data_sanity.py
@@ -9,6 +9,7 @@ import os
 def suite():
     return (
         check_for_empty_slds,
+        check_for_missing_latlng,
         )
 
 
@@ -28,8 +29,20 @@ def extension(f):
     return os.path.splitext(f)[1]
 
 
+def files_matching_extension(ext):
+    return scan_geoserver_data_dir(lambda x: extension(x) == ext)
+
+
 @check(email_on_error=True)
 def check_for_empty_slds():
-    for f in scan_geoserver_data_dir(lambda x: extension(x) == '.sld'):
+    for f in files_matching_extension('.sld'):
         if os.stat(f).st_size == 0:
             raise CheckFailed('Empty sld: %s' % f)
+
+
+@check(email_on_error=True)
+def check_for_missing_latlng():
+    for f in scan_geoserver_data_dir(lambda x: x.endswith('featuretype.xml')):
+        with open(f) as f:
+            if 'latLonBoundingBox' not in f.read():
+                raise CheckFailed('Missing latLonBoundingBox in %s' % f.name)
