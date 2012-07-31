b7730513689ff91d0477acf0ebe83504719e3de8
Robert Marianski <rob@marianski.com>
2012-07-31 14:58:20 -0400

    add watchdog summary support

--- management/commands/watchdog.py
+++ management/commands/watchdog.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+from datetime import timedelta
 from django.core.management.base import BaseCommand
 from mapstory.watchdog.core import _run_watchdog_suites
 from mapstory.watchdog.core import clean_runs
 from mapstory.watchdog.core import list_suites
+from mapstory.watchdog.core import summarize
 from mapstory.watchdog.logs import set_log_state_to_end_of_file
 import sys
 
@@ -9,7 +12,7 @@ import sys
 class Command(BaseCommand):
     help = 'Watchdog functions'
     args = ('mark_log_ok | list | clean_runs [older-than-days] '
-            '| [suite...]')
+            '| summarize (day|week) | [suite...]')
 
     def handle(self, *args, **opts):
         if not args:
@@ -28,5 +31,26 @@ class Command(BaseCommand):
                 except ValueError:
                     pass
             clean_runs(ndays)
+        elif args[0] == 'summarize':
+            usage = "must specify 'day' or 'week' to summarize"
+            if len(args) == 1:
+                print usage
+                return
+            else:
+                period = args[1]
+                if period not in ('day', 'week'):
+                    print usage
+                    return
+                days = 1
+                if period == 'day':
+                    days = 1
+                elif period == 'week':
+                    days = 7
+                now = datetime.now()
+                delta = timedelta(days=days)
+                plain_day = datetime(now.year, now.month, now.day)
+                start_time = plain_day - delta
+                end_time = plain_day
+                summarize(start_time, end_time)
         else:
             _run_watchdog_suites(*args)
