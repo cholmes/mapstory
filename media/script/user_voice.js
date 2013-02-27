f5871219c7d950d88db418ecc43b785d7e56fa16
Ian Schneider <planablediglet@gmail.com>
2013-02-27 13:36:54 -0700

    uservoice widget update

--- media/script/user_voice.js
+++ media/script/user_voice.js
@@ -1,6 +1,23 @@
-var uvOptions = {};
-(function() {
-  var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-  uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/DhyHXis6WVw0y7RC7hHUNw.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
-})();
+// old way??
+//var uvOptions = {};
+//(function() {
+//  var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+//  uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/DhyHXis6WVw0y7RC7hHUNw.js';
+//  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+//})();
+
+// new way??
+(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/gu0AM9xKZuBLDfJArcA.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
+
+UserVoice = window.UserVoice || [];
+UserVoice.push(['showTab', 'classic_widget', {
+  mode: 'full',
+  primary_color: '#cc6d00',
+  link_color: '#007dbf',
+  default_mode: 'support',
+  forum_id: 189612,
+  tab_label: 'Feedback & Support',
+  tab_color: '#cc6d00',
+  tab_position: 'middle-right',
+  tab_inverted: false
+}]);
